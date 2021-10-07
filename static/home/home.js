0f39d451cff361f03729070382ae361458208680
shivamsuyal <shivamsuyal25@gmail.com>
2021-10-07 11:36:40 +0530

    mode button

--- static/home/home.js
+++ static/home/home.js
@@ -23,3 +23,25 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+// new 2
+
+function chgmode(evt){
+  sun=document.querySelector('.sun')
+  moon=document.getElementsByClassName('moon')
+  mode=document.getElementById('mode')
+  
+  if (getComputedStyle(sun)['visibility'] =="visible"){
+    sun.style="visibility:hidden;transform: translateY(2rem);"
+    moon[0].style="visibility:visible;transform: translateY(0);"
+    mode.style="background-color:#8B86D3";
+    // moon[1].style="visibility:visible;transform: translateY(0);"
+  }else{
+    sun.style="visibility:visible;transform: translateY(0rem);"
+    moon[0].style="visibility:hidden;transform: translateY(-2rem);"
+    mode.style="background-color:#0584C3";
+    // moon[1].style="visibility:hidden;transform: translateY(-2rem);"
+  }
+}
+
+
